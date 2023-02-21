1e978f4adc3a0d3378f11ae4a800e8d7b43cf6ab
JuanmaRamosRodriguez <95094039+JuanmaRamosRodriguez@users.noreply.github.com>
2023-02-21 13:46:35 +0100

    update

--- ProjectoAniku/src/config.js
+++ ProjectoAniku/src/config.js
@@ -1,5 +1,29 @@
 const port = 3000;
+// eslint-disable-next-line no-use-before-define
+const user = user;
+// eslint-disable-next-line no-use-before-define
+const pass = pass;
+// eslint-disable-next-line no-use-before-define
+const host = host;
+// eslint-disable-next-line no-use-before-define
+const dbName = dbName;
+// eslint-disable-next-line no-use-before-define
+const app = app;
 
 module.exports = {
   port,
 };
+
+const mongodb = {
+  user: process.env.DATABASE_USER,
+  pass: process.env.DATABASE_PASS,
+  host: process.env.DATABASE_HOST,
+  dbName: process.env.DATABASE_NAME,
+};
+
+const config = {
+  app,
+  mongodb,
+};
+
+module.exports = config;
