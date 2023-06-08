8884d069d6c438b9077214b10866320c8dd033db
JuanmaRamosRodriguez <95094039+JuanmaRamosRodriguez@users.noreply.github.com>
2023-06-08 11:52:38 +0200

    update proyecto
    instalación cors

--- ProjectoAniku/src/app.js
+++ ProjectoAniku/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const loaders = require('./loaders');
 const config = require('./config');
 
 const app = express();
-
+app.use(cors());
 loaders.init(app, config);
 
 module.exports = app;
