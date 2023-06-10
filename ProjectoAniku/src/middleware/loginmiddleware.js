79207bd49e03ae7e1adea350ae1086135a4b6026
JuanmaRamosRodriguez <95094039+JuanmaRamosRodriguez@users.noreply.github.com>
2023-06-10 13:41:25 +0200

    update
    pruebaa

--- ProjectoAniku/src/middleware/loginmiddleware.js
+++ ProjectoAniku/src/middleware/loginmiddleware.js
@@ -1,33 +1,20 @@
-const jwt = require('jsonwebtoken');
 const Usuarios = require('../models/usuario');
-
-const secret = 'admin';
 
 const loginMiddleware = async (req, res, next) => {
   // eslint-disable-next-line no-console
   console.log(req.headers.authorization);
-  const token1 = req.headers.authorization;
 
-  if (token1 === undefined) {
-    return res.status(401).json({ message: 'No se proporcionó el token para autentificar.' });
+  // Aquí puedes agregar tu lógica de autenticación sin token
+
+  const usuarios = await Usuarios.findOne({ /* lógica de búsqueda del usuario */ });
+
+  if (!usuarios) {
+    return res.status(401).json({ message: 'Usuario no registrado.' });
   }
-  const token = req.headers.authorization.substring(7);
 
-  try {
-    const decodedToken = jwt.verify(token, secret);
+  req.usuarios = usuarios;
 
-    const usuarios = await Usuarios.findById(decodedToken.usuariosId);
-
-    if (!usuarios) {
-      return res.status(401).json({ message: 'Usuario no registrado.' });
-    }
-
-    req.usuarios = usuarios;
-
-    return next();
-  } catch (err) {
-    return res.status(401).send({ message: 'El token de autenticación no es válido.' });
-  }
+  return next();
 };
 
 module.exports = {
