b3e2485da7d2bbafadd0225dd154d9dd4e20e0d1
JuanmaRamosRodriguez <95094039+JuanmaRamosRodriguez@users.noreply.github.com>
2023-06-10 18:36:53 +0200

    update
    otra prueba

--- ProjectoAniku/src/routes/login.js
+++ ProjectoAniku/src/routes/login.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { loginUser } = require('../controllers/login');
 
 const router = express.Router();
-router.post('/', loginUser);
+router.post('/login', loginUser);
 
 module.exports = router;
